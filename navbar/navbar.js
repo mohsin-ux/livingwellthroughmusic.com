8f07e9857bbfce0fe104c82ef641513a80f04d18
Mohsin-Raziii <mohsinrazakhan4455@gmail.com>
2023-07-16 16:49:36 +0500

    mobile

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,7 +1,12 @@
-const menu = document.querySelector(".menu");
-const nav = document.querySelector(".nav");
+// const menu = document.querySelector(".menu");
+// const nav = document.querySelector(".nav");
 
-menu.addeventlistner("click", () => {
-  menu.classList.toggle("icon");
-  nav.classList.toggle("change");
-});
+// menu.addeventlistner("click", () => {
+//   menu.classList.toggle("icon");
+//   nav.classList.toggle("change");
+// });
+
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("icon");
+  document.getElementById("nav").classList.toggle("change" );
+}
